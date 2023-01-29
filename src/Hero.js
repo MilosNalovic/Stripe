dc226345d40203a6efd2136a7d70a493954b1d9b
Miloš Nalović <milos.nalovic@gmail.com>
2023-01-29 21:15:52 +0100

    hero section added

--- src/Hero.js
+++ src/Hero.js
@@ -3,9 +3,25 @@ import phoneImg from "./images/phone.svg";
 import { useGlobalContext } from "./context";
 
 const Hero = () => {
-  const data = useGlobalContext();
-  console.log(data);
-  return <h2>hero component</h2>;
+  const { closeSubmenu } = useGlobalContext();
+  return (
+    <section className="hero">
+      <div className="hero-center">
+        <article className="hero-info">
+          <h1>Payments infrastucture for the internet</h1>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet a
+            dolore magnam! Nulla vitae corrupti consequatur corporis molestias
+            deserunt? Odio.
+          </p>
+          <button className="btn">Start now</button>
+        </article>
+        <article className="hero-images">
+          <img src={phoneImg} className="phone-img" alt="phone img" />
+        </article>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
